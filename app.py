361d667ce7bfa7e11fb8d7e921308ad8a160f9da
Pallavinehaa2003 <pallavisahoo263@gmail.com>
2025-04-20 11:33:08 +0530

    new book recommender

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def results():
     recommendations = get_recommendations(responses)
     return render_template('result.html', recommendations=recommendations)
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
